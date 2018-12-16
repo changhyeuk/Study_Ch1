52047b61d019091c02d4b0d5b21c22caf4f1d429
ChangKimMac <changkim@cpe-2606-a000-111c-81ef-4c5-d82e-4e81-a87e.dyn6.twc.com>
2018-12-16 11:17:48 -0500

    Initial Commit

---
+++ Ch1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ch1
+//
+//  Created by ChangKimMac on 12/16/18.
+//  Copyright © 2018 ChangKim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
