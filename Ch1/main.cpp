1a922a79b339f0dbc72a8cfad902eb4659f420cb
ChangKimMac <changkim@cpe-2606-a000-111c-81ef-19fc-69a5-c846-96f0.dyn6.twc.com>
2018-12-17 16:09:34 -0500

    BetweenAdder

--- Ch1/main.cpp
+++ Ch1/main.cpp
@@ -10,16 +10,25 @@
 
 int main(void)
 {
-    int val1;
-    std::cout<<" The first number : ";
-    std::cin>>val1;
+    int val1, val2;
+    int result=0;
+    std::cout<<" Input two numbers : ";
+    std::cin>>val1>>val2;
     
-    int val2;
-    std::cout<<" The second number : ";
-    std::cin>>val2;
-    
-    int result = val1 + val2;
-    std::cout<<" The Sum will be : "<< result <<std::endl;
+    if (val1<val2)
+    {
+        for ( int i = val1+1; i<val2; i++)
+        {
+            result+= i;
+        }
+    }
+    else
+    {
+        for ( int i = val2+1; i<val1 ; i++)
+        {
+            result+= i;
+        }
+    }
+    std::cout<<" The sum between two integer "<<result<<std::endl;
     return 0;
-
 }
