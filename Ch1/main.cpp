1d81b64e8b02bb8b2f15babda276ce2df6224a23
ChangKimMac <changkim@changkimmac.local>
2018-12-18 14:20:34 -0500

    using string

--- Ch1/main.cpp
+++ Ch1/main.cpp
@@ -10,25 +10,17 @@
 
 int main(void)
 {
-    int val1, val2;
-    int result=0;
-    std::cout<<" Input two numbers : ";
-    std::cin>>val1>>val2;
+    char name[100];
+    char lang[200];
     
-    if (val1<val2)
-    {
-        for ( int i = val1+1; i<val2; i++)
-        {
-            result+= i;
-        }
-    }
-    else
-    {
-        for ( int i = val2+1; i<val1 ; i++)
-        {
-            result+= i;
-        }
-    }
-    std::cout<<" The sum between two integer "<<result<<std::endl;
+    std::cout<<" What is your name : ";
+    std::cin>>name;
+    
+    std::cout<<" What is the favorite language : ";
+    std::cin>>lang;
+    
+    std::cout<<" My name is "<< name << std::endl;
+    std::cout<<" My favorite lange is "<< lang <<std::endl;
+    
     return 0;
 }
