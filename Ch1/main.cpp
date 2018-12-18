2e13da5991c9de7086b2ae87f112566d1348dfcd
ChangKimMac <changkim@cpe-2606-a000-111c-81ef-880b-4e7-9465-b16e.dyn6.twc.com>
2018-12-18 15:59:56 -0500

    Pointer Explation

--- Ch1/main.cpp
+++ Ch1/main.cpp
@@ -8,45 +8,11 @@
 
 #include <iostream>
 
-void swap(int* a, int* b)
+int main()
 {
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void swap(char* a, char* b)
-{
-    char temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void swap(double* a, double* b)
-{
-    double temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-int main(void)
-{
-    int num1 = 20, num2=30;
-    std::cout<< num1 << ' ' << num2 <<std::endl;
-    swap(&num1, &num2);
-    std::cout<< num1 <<' '<< num2<<std::endl;
-    
-    char ch1 = 'A', ch2 = 'Z';
-    std::cout<< ch1 << ' ' << ch2 <<std::endl;
-    swap(&ch1, &ch2);
-    std::cout<< ch1 << ' ' << ch2<<std::endl;
-    
-    double dbl1=1.111, dbl2=5.555;
-    swap(&dbl1, &dbl2);
-    std::cout<< dbl1 << ' ' << dbl2 << std::endl;
-    
+    int x = 5;
+    std::cout<< x <<'\n';
+    std::cout<< &x <<'\n';
+    std::cout<< *&x <<'\n';
     return 0;
 }
